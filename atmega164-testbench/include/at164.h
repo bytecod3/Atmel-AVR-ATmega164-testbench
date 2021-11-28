2768288ffcb962bfd18c226ad08d4203c0ffe7d9
bytecod3 <emwiti658@gmail.com>
2021-11-29 01:21:37 +0300

    add int to char conversion

--- atmega164-testbench/include/at164.h
+++ atmega164-testbench/include/at164.h
@@ -1,6 +1,22 @@
 #ifndef AT164 _H
 #define AT164 _H
 
+void initialize_ports(void){
+    // set PORTB as input
+    unsigned char * DDRB, *DDRC;
+    unsigned char * PORTB, *PORTC;
+    DDRB = 0xff; //  input
+    PORTB = (unsigned char) 0x25;   // intitialize LOW 
+    PORTB = 0x00;
 
+    // set PORT C as output
+    DDRC = 0x00;
+    PORTC = (unsigned char) 0x28;
+    
+    PORTC = 0x00; // initialize low
+
+
+    
+}
 
 #endif
